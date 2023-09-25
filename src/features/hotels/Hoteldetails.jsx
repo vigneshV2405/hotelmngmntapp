2d2210172bf2f463b03de96f5eab5d0a2eee3983
vigneshV2405 <vigneshvarikolu111@gmail.com>
2023-09-25 23:04:11 +0530

    dfgh

--- src/features/hotels/Hoteldetails.jsx
+++ src/features/hotels/Hoteldetails.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useGethotelbyIdQuery } from '../../services/Hotels'
+import _ from 'lodash';
 
 function Hoteldetails() {
     var { id } = useParams()
     var { data , isLoading } = useGethotelbyIdQuery(id)
-    console.log(data)
+    //var [ roomsByCategory , setRoomsbycategory ] = useState(null)
+    var roomsByCategory = {}
+    if(!isLoading){
+      //setRoomsbycategory(_.groupBy(data.rooms,'roomType'))
+      roomsByCategory = _.groupBy(data.rooms,'roomType')
+    }
+    
   return (
     <div>
-        
+        {
+          !isLoading && 
+          <>
+            <h1>{data.hotelName}</h1>
+            <i>Location: {data.area}</i>
+            <h4>Rooms:</h4>
+            {Object.keys(roomsByCategory).map((category)=>{
+              return (
+                <h5>
+                  {category}:
+                  {
+                    data.rooms.map((room)=>{
+                      if(room.roomType===category){
+                        return <i>{room.roomId}</i>
+                      }
+                    })
+                  }
+                </h5>
+              )
+            })}
+          </>
+        }
     </div>
   )
 }
